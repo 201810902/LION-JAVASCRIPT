b2d6b71cd77445a9b8b350bd9f84d16516f8b907
Kyounga <kacomtr@gamil.com>
2023-07-03 14:19:11 +0900

    first commit

---
+++ server/index.js
@@ -0,0 +1,12 @@
+console.log("hello js");
+
+const liveServer = require('live-server');
+
+const params={
+  port:5500,
+  host: 'localhost',
+  root: './client',
+  open:false
+}
+
+liveServer.start(params);
